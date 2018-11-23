e6e6cfad397afdb21fa68171bf7a7ca15283c9e7
mtsylko <mtsylko@gmail.com>
2018-11-23 18:42:04 +0300

    radio styles

--- src/component/form/radio/Radio.js
+++ src/component/form/radio/Radio.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import './Radio.scss'
 
 const Radio = ({ name, value, label, onChange, isActive }) => {
-  return <div>
+  return <div className="radio-group radio">
     <label htmlFor={name}>{label}</label>
     <input type="radio" name={name} value={value} checked={isActive} onChange={onChange}/>
   </div>
