0ec375455c789148af400811d6e585b498192e7c
mtsylko <mtsylko@gmail.com>
2018-12-07 18:48:15 +0300

    form changes

--- src/component/form/dropdown/Dropdown.js
+++ src/component/form/dropdown/Dropdown.js
@@ -6,7 +6,7 @@ export default class Dropdown extends React.PureComponent {
   render() {
     const { name, value, options, onChange  } = this.props;
     return <div className="dropdown">
-      <select name={name} onChange={onChange} value={value}>
+      <select name={name} onChange={onChange} defaultValue={value}>
         {options.map(item => {
             return <option key={item.value} value={item.value}>{item.label}</option>
         })}
